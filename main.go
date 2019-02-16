0116af214dad974002e80ac2ff6e7028fe5fc422
Remco Beckers <remco.beckers@gmail.com>
2019-02-16 22:57:25 +0100

    Add password protection

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/subtle"
 	"flag"
 	"fmt"
 	"io"
@@ -10,6 +11,7 @@ import (
 	"net/http"
 	"net/http/httputil"
 	"net/url"
+	"strings"
 	"time"
 
 	"github.com/rb3ckers/trafficmirror/datatypes"
@@ -24,7 +26,8 @@ var targets = datatypes.NewMirrorTargets()
 func main() {
 	listenPort := flag.String("port", "7071", "Port to listen on")
 	proxyTarget := flag.String("main", "http://localhost:7072", "Main proxy target, its responses will be returned")
-	mirrorsEndpoint := flag.String("mirrors", "mirrors", "Path on which additional mirrors can be added/deleted/listed")
+	mirrorsEndpoint := flag.String("targets", "targets", "Path on which additional targets to mirror to can be added/deleted/listed")
+	passwordFile := flag.String("password", "", "Provide a file that contains username/password to protect the configuration 'targets' endpoint. Contains 1 username/password combination separated by '\n'.")
 
 	help := flag.Bool("help", false, "Print help")
 
@@ -35,7 +38,7 @@ func main() {
 		fmt.Printf("* sends requests to a main endpoint from which the response is returned")
 		fmt.Printf("* can mirror the requests to any additional number of endpoints")
 		fmt.Printf("")
-		fmt.Printf("Additional targets are configured via POST/DELETE on the `/mirrorTargets?url=<endpoint>`.")
+		fmt.Printf("Additional targets are configured via POST/DELETE on the `/targets?url=<endpoint>`.")
 
 		flag.PrintDefaults()
 		return
@@ -45,7 +48,12 @@ func main() {
 
 	proxyTo := httputil.NewSingleHostReverseProxy(url)
 
-	http.HandleFunc("/"+*mirrorsEndpoint, mirrorsHandler)
+	if *passwordFile != "" {
+		username, password := parseUsernamePassword(*passwordFile)
+		http.HandleFunc("/"+*mirrorsEndpoint, BasicAuth(mirrorsHandler, username, password, "Please provide username and password for changing mirror targets"))
+	} else {
+		http.HandleFunc("/"+*mirrorsEndpoint, mirrorsHandler)
+	}
 
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		body := bufferRequest(req)
@@ -119,4 +127,43 @@ func mirrorsHandler(res http.ResponseWriter, req *http.Request) {
 		log.Printf("Removing '%s' from targets list.", targetURLs)
 		targets.Delete(targetURLs)
 	}
+}
+
+// BasicAuth wraps a handler requiring HTTP basic auth for it using the given
+// username and password and the specified realm, which shouldn't contain quotes.
+// From StackOverflow: https://stackoverflow.com/questions/21936332/idiomatic-way-of-requiring-http-basic-auth-in-go/39591234#39591234
+//
+// Most web browser display a dialog with something like:
+//
+//    The website says: "<realm>"
+//
+// Which is really stupid so you may want to set the realm to a message rather than
+// an actual realm.
+func BasicAuth(handler http.HandlerFunc, username, password, realm string) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		user, pass, ok := r.BasicAuth()
+
+		if !ok || subtle.ConstantTimeCompare([]byte(user), []byte(username)) != 1 || subtle.ConstantTimeCompare([]byte(pass), []byte(password)) != 1 {
+			w.Header().Set("WWW-Authenticate", `Basic realm="`+realm+`"`)
+			w.WriteHeader(401)
+			w.Write([]byte("Unauthorised.\n"))
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
+func parseUsernamePassword(passwordFile string) (string, string) {
+	data, err := ioutil.ReadFile(passwordFile)
+	if err != nil {
+		panic("Failed to load password file.")
+	}
+	split := strings.SplitN(string(data), "\n", 2)
+	if len(split) != 2 {
+		panic("Failed to parse username/password. Expected 2 find username/password separated by a new line.")
+	}
+	return split[0], split[1]
 }
