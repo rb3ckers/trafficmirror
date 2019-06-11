98d12e3b3af659465a0ee4a1ff94814e3efc02eb
Remco Beckers <remco.beckers@gmail.com>
2019-06-11 14:45:58 +0200

    Sync listen port to exposed docker port

--- init.sh
+++ init.sh
@@ -2,11 +2,11 @@
 
 # Takes these environment variables:
 #
-# LISTEN_PORT: port to listen on (defaults to 7077)
+# LISTEN_PORT: port to listen on (defaults to 8080)
 # MAIN: reverse proxy to this address (defaults to localhost:8888)
 # USERNAME & PASSWORD: if USERNAME is set protect targets endpoint with basic auth (default to empty)
 
-listen_port=${LISTEN_PORT:-7077}
+listen_port=${LISTEN_PORT:-8080}
 main=${MAIN:-localhost:8888}
 extra_params=$1
 
